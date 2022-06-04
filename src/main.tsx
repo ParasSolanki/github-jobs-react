66f5b27b4e753d9e5f983f6bd64448d5df7de98d
parasSolanki <parassolanki151@gmail.com>
2022-06-04 13:44:57 +0530

    added search form

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from 'preact'
 import { App } from './app'
 import '@/styles/style.css'
+import '@/styles/custom.css'
 
 render(<App />, document.getElementById('app')!)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,20 @@
 const SearchForm = () => {
   return (
-    <div className="container mx-auto px-4 h-44">
-      <div className="w-full h-full relative">
-        <picture className="absolute inset-0 object-cover">
-          <source
-            srcset={
-              new URL(
-                '../assets/images/building-background-webp.webp',
-                import.meta.url
-              ).href
-            }
-            type="image/webp"
-          />
-          <source
-            srcset={
-              new URL(
-                '../assets/images/building-background-jpg.jpg',
-                import.meta.url
-              ).href
-            }
-            type="image/jpeg"
-          />
-          <img
-            className="absolute inset-0 w-full h-full object-cover"
-            src={
-              new URL(
-                '../assets/images/building-background-jpg.jpg',
-                import.meta.url
-              ).href
-            }
-            alt="Alt Text!"
-          />
-        </picture>
-      </div>
-      <div className="flex items-center justify-center ">
-        <form className="max-w-6xl mx-auto"></form>
-      </div>
+    <div className="container mx-auto px-4 h-44 flex items-center justify-center search-container">
+      <form className="max-w-4xl w-full mx-auto flex items-center justify-center space-x-3 sm:space-x-5">
+        <input
+          type="search"
+          name="search"
+          className="py-3 px-3 w-full bg-white text-base text-slate-900 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+        />
+
+        <button
+          type="submit"
+          className="py-3 px-5 text-base bg-blue-600 text-white rounded focus:outline-none focus:ring-2 focus:ring-white"
+        >
+          Search
+        </button>
+      </form>
     </div>
   )
 }
