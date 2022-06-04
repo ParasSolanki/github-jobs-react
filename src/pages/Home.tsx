bcb7d8869f6fa5de90ac9008feacb2fac9c7efd7
parasSolanki <parassolanki151@gmail.com>
2022-06-04 14:11:12 +0530

    added search input form handling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
-const SearchForm = () => {
+import { FunctionalComponent } from 'preact'
+import { StateUpdater, useState } from 'preact/hooks'
+
+interface SearchFormProps {
+  searchTerm: string
+  setSearchTerm: StateUpdater<string>
+}
+
+const SearchForm: FunctionalComponent<SearchFormProps> = ({
+  searchTerm,
+  setSearchTerm,
+}) => {
   return (
     <div className="container mx-auto px-4 h-44 flex items-center justify-center search-container">
       <form className="max-w-4xl w-full mx-auto flex items-center justify-center space-x-3 sm:space-x-5">
         <input
           type="search"
           name="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.currentTarget.value)}
           className="py-3 px-3 w-full bg-white text-base text-slate-900 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
         />
 
@@ -20,6 +33,7 @@ const SearchForm = () => {
 }
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <main>
       <div className="container mx-auto px-4 py-8">
@@ -28,7 +42,7 @@ const Home = () => {
         </h1>
       </div>
 
-      <SearchForm />
+      <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
     </main>
   )
 }
